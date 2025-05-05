7162763d1e6704a8b995354407f825ce08145a6a
developer-shourav <mrinmoyraj823@gmail.com>
2025-05-05 18:53:54 +0600

    Dynamic SSG completed

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,16 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+/* ------------For Pre Static Build like SSG (Static Site Generation)--------------- */
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return blogs?.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
 
 const BlogDetails = async ({
   params,
