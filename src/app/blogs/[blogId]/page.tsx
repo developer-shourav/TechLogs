170c7eebeb827332b4a9392daf60cd73c3c982e7
developer-shourav <mrinmoyraj823@gmail.com>
2025-05-08 16:31:29 +0600

    Metadata added

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -12,6 +12,34 @@ export const generateStaticParams = async () => {
   }));
 };
 
+/* ----------------Generate Dynamic Metadata--------------- */
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+  return {
+    title: blog?.title,
+    description: blog?.description,
+    openGraph: {
+      title: blog.title,
+      description: blog.description,
+      images: [
+        {
+          url: blog.blog_image || "/default-og.png",
+          width: 1200,
+          height: 630,
+          alt: blog.title,
+        },
+      ],
+    },
+  };
+}
+
 const BlogDetails = async ({
   params,
 }: {
